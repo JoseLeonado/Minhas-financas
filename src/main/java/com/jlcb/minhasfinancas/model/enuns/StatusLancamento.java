a2131b5e4a97636bcaa262b9063f9180074dc0fd
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-05-23 11:41:38 -0300

    Criando e mapeando a entidade de Usuario e Lancamento

---
+++ src/main/java/com/jlcb/minhasfinancas/model/enuns/StatusLancamento.java
@@ -0,0 +1,8 @@
+package com.jlcb.minhasfinancas.model.enuns;
+
+public enum StatusLancamento {
+	
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+}
---
+++ src/main/java/com/jlcb/minhasfinancas/model/enuns/TipoLancamento.java
@@ -0,0 +1,7 @@
+package com.jlcb.minhasfinancas.model.enuns;
+
+public enum TipoLancamento {
+	
+	RECEITA,
+	DESPESA
+}
