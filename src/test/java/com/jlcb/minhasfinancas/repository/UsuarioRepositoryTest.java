19ca28dd5823c7bdf71804490ed6bc1864688704
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-05-23 23:38:26 -0300

    Refatorando o teste de Usuario Repository

--- src/test/java/com/jlcb/minhasfinancas/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/jlcb/minhasfinancas/repository/UsuarioRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,13 +23,16 @@ public class UsuarioRepositoryTest {
 	
 	@Autowired
 	UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	TestEntityManager entityManager;
 
 	@Test
 	public void deveVerificarAExistenciaDeUmEmail() {
 		
 		/* Cenário */
 		Usuario usuario = Usuario.builder().nome("Usuário").email("usuario@email.com").build();
-		usuarioRepository.save(usuario);
+		entityManager.persist(usuario);
 		
 		/* Ação */
 		boolean resultado = usuarioRepository.existsByEmail("usuario@email.com");
@@ -41,7 +45,6 @@ public class UsuarioRepositoryTest {
 	public void deveRetornarFasloQuandoNaoHouverUsuarioComOEmail() {
 		
 		/* Cenário */
-		usuarioRepository.deleteAll();
 		
 		/* Ação */
 		boolean resultado = usuarioRepository.existsByEmail("usuario@email.com");
